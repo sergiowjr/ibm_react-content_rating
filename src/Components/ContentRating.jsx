10371c0d2e3f387c2e967f72cbac5405abd7220d
Balevicky <balevictorien@yahoo.fr>
2024-08-12 03:42:10 -0400

    Changed three times ContentRating.jsx

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -7,7 +7,17 @@ class ContentRating extends Component {
     super();
     this.state= {
         likes: 0,
-        dislikes: 0 
+        dislikes: 0,
+        handlelike:() => {
+            this.setState((prevstate) => ({
+                likes: prevstate.likes+1
+            }))
+        },
+        handledislike:()=> {
+            this.setState((prevstate) => ({
+                dislikes: prevstate.dislikes+1
+            }))
+        }
     }
   }
   render() {
@@ -16,10 +26,10 @@ class ContentRating extends Component {
     <div className='content-rating'>
          <p>Add text here</p>
          <div className='rating-buttons'>
-           <button className='like-button'>
+           <button className='like-button' onClick={this.state.handlelike}>
             Like ({this.state.likes})
             </button>
-            <button className='dislike_button'>
+            <button className='dislike-button' onClick={this.state.handledislike}>
             Dislike ({this.state.dislikes})
             </button>
           </div>
