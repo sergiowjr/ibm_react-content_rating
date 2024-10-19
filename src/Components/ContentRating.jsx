ee81f0844a12a5f1d791b85d3227b53835eb301a
Frumosi <kkone.devops@gmail.com>
2024-10-19 14:25:50 -0400

    updated

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,22 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+        likes: 0,
+        dislikes: 0
+      };
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+     <div className='content-rating'>
+        <p>
+        JSX JavaScript XML syntax is similar to HTML, 
+        and it is used by React to describe what the UI 
+        should look like. It extends ECMAScript by allowing 
+        HTML-like code to be written within JavaScript, 
+        which gets transpiled to React.createElement calls.
+        </p>
+     </div>
     );
   }
 }
