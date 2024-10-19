b94dea55b1c0f431d29521bf61aafb4f7ed8155f
Frumosi <kkone.devops@gmail.com>
2024-10-19 14:33:42 -0400

    updated

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import './ContentRating.css';
 
@@ -6,23 +5,41 @@ class ContentRating extends Component {
   constructor() {
     super();
     this.state = {
-        likes: 0,
-        dislikes: 0
-      };
+      likes: 0,
+      dislikes: 0,
+      totalRatings: 0,
+      handleLike: () => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      },
+      handleDislike: () => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      }
+    };
   }
   render() {
     return (
-     <div className='content-rating'>
-        <p>
-        JSX JavaScript XML syntax is similar to HTML, 
-        and it is used by React to describe what the UI 
-        should look like. It extends ECMAScript by allowing 
-        HTML-like code to be written within JavaScript, 
-        which gets transpiled to React.createElement calls.
-        </p>
-     </div>
+      <>
+        <h1>Text Content Rating</h1>
+        <div className='content-rating'>
+          <p>Text</p>
+          <div className='rating-buttons'>
+            <button className="like-button" onClick={this.state.handleLike}>
+              Like ({this.state.likes})
+            </button>
+            <button className="dislike-button" onClick={this.state.handleDislike}>
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+          <p>Total Ratings: {this.state.totalRatings}</p>
+        </div>
+      </>
     );
   }
 }
-
 export default ContentRating;
